87ef5a8ba36582b40309a0e68333ba67e19451c1
lady.chesha@yandex.ru <ladi.chesha@yandex.ru>
2019-04-03 22:21:11 +0500

    диагональ

---
+++ vera1.js
@@ -0,0 +1,12 @@
+var readline=require("readline-sync");
+var n = readline.question(" n :");
+for ( var i=0; i<n; i++){
+    for(var j=0;j<n;j++){
+        if(i==n-j-1){
+            process.stdout.write("*");
+        }else{
+            process.stdout.write(".");
+        }
+    }
+    process.stdout.write("\n");
+}
