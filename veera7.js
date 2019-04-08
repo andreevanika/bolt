ff2bbb494b845154399d53461aa94074f004fb3e
lady.chesha@yandex.ru <ladi.chesha@yandex.ru>
2019-04-08 22:48:53 +0500

    for if

---
+++ veera7.js
@@ -0,0 +1,13 @@
+var readline=require("readline-sync");
+var n = readline.question(" n :");
+for ( var i=0; i<n; i++){
+    for(var j=0;j<n;j++){
+        if(((i<=n-j-1) && (i>=j))|| ((i<=j) && (i>=n-j-1)) ){
+            
+            process.stdout.write("*");
+        }else{
+            process.stdout.write(".");
+        }
+    }
+    process.stdout.write("\n");
+}
---
+++ vera8.js
@@ -0,0 +1,13 @@
+var readline=require("readline-sync");
+var n = readline.question(" n :");
+for ( var i=0; i<n; i++){
+    for(var j=0;j<n;j++){
+        if(((j<=n-i-1) && (j>=i))|| ((j<=i) && (j>=n-i-1)) ){
+            
+            process.stdout.write("*");
+        }else{
+            process.stdout.write(".");
+        }
+    }
+    process.stdout.write("\n");
+}
